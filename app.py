5deefd6fe99602b8336301c1f5accd9c65e80c4a
D0sal <57319456+D0sal@users.noreply.github.com>
2023-04-06 08:39:35 +0000

    update app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 from flask import Flask, jsonify
-
+% from your_file import your_function
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,17 +10,18 @@ def index():
 
 @app.route('/single/<text>', methods=['GET'])
 def generate_single_image(text):
-    image = text
+    # image_paths = python_script(text)
+    # closest_image = image_paths[0]
 
     # return the image as a response
-    return jsonify(image=image)
+    return jsonify(image=text)
 
 @app.route('/multiple/<text>', methods=['GET'])
 def generate_multiple_images(text):
     images = text
 
-    # return the image as a response
-    return jsonify(images=images)
+    # return the images as a response
+    return jsonify(images=text)
 
 if __name__ == '__main__':
     app.run(debug=True)
